36b41929aa22d804ad9fc418f724109619633d9b
jesswinw <jesswinw@gmail.com>
2019-12-09 20:51:12 +0530

    [bulding stage]A small number guessing game

---
+++ Guess the Number/guess.py
@@ -0,0 +1,12 @@
+import random
+
+print("""Guess the Number
+Instructions:
+1.The computer guesses a number b/w 1 to 100
+2.You have only 30 chances
+3.Enter q to quit
+
+---Best of Luck---""")
+
+while(1):
+    pass
