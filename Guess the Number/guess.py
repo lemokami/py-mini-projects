78477618d74149f7fc7ae1fd39e0a2d554688fc0
jesswinw <jesswinw@gmail.com>
2019-12-10 18:58:22 +0530

    minor changes (bugs added)

--- Guess the Number/guess.py
+++ Guess the Number/guess.py
@@ -12,15 +12,24 @@ chnce=30
 num = random.randint(1,100)
 
 print("I Guessed a Number,",end='')
-
+pdif=100
 while(chnce):
     try:
         gus = int(input("Enter:>"))
     except:break
     chnce-=1
+    
+    print(pdif)
     if(gus==num):
         print("Wonderful Job")
         break
+    elif(abs(gus-num)<=pdif):
+        print("hotter")
+    elif(abs(gus-num)<=10):
+        print("hot")
+    else:
+        print("cold")
     print(f"You have {chnce} chances to go\n")
+    pdif = abs(gus-num)
 if(chnce==0):
     print(f"Number was {num}")
