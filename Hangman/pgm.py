8fdb5e0a286ae755a14f5677941762e318322b0b
jesswinw <jesswinw@gmail.com>
2019-12-12 22:31:36 +0530

    Made some more changes

--- Hangman/pgm.py
+++ Hangman/pgm.py
@@ -1,11 +1,40 @@
-import random
+import random,os
+
+ch = int(input("Whats your OS\n1.WIN\t2.LINUX\n:>"))
+
+if(ch==1):
+    fn="cls"
+else:
+    fn="clear"
+
+os.system(fn)    
+
 print("""----WELCOME TO HANGMAN----
 Instructions:
 1.The computer guesses a word from the words text
 2.You will get the chances till the man dies
 """)
+input()
+os.system(fn)
 
 with open("Hangman/words.txt") as f:
-    wordlist = [x[:-1] for x in f]
+    wordlist = [x[:-1] for x in f if len(x)>=2]
 
-print(wordlist[random.randint(0,len(wordlist))])
+word = wordlist[random.randint(0,len(wordlist))]
+
+print(f"""I Guessed a word. What is it?
+It has {len(word)} letters.
+Guess it before your choices make him the
+H A N G M A N
+{'_ '*len(word)}""")
+
+print(word)
+
+# This is how a hangman looks like
+# ___________
+# |     |
+# |     0
+# |    /|\\
+# |    / \   
+# |
+# -----------
