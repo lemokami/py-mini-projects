ee629cb25acec6518b5947fd7191ea0e85b3bc81
jesswinw <jesswinw@gmail.com>
2019-12-12 19:07:11 +0530

    Added some stuff

--- Hangman/pgm.py
+++ Hangman/pgm.py
@@ -1,1 +1,11 @@
-print("Welcome to Hangman")
+import random
+print("""----WELCOME TO HANGMAN----
+Instructions:
+1.The computer guesses a word from the words text
+2.You will get the chances till the man dies
+""")
+
+with open("Hangman/words.txt") as f:
+    wordlist = [x[:-1] for x in f]
+
+print(wordlist[random.randint(0,len(wordlist))])
